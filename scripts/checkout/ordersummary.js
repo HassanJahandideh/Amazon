2add80340ff1e3e746b14168ad5a04a312bce5d5
Hassan Jahandideh <jahandideh.2083@gmail.com>
2024-06-08 15:18:22 +0300

    make payment summary interactive

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -3,10 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrancy } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -109,6 +106,8 @@ export function renderOrderSummary() {
     
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
